88bda9760a7c1bd00c8dda9043920909520f171a
ruoshui <rmao10@stuy.edu>
2020-07-23 14:50:30 -0400

    fix(persp): transpose matrix

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,8 @@
 mod graphics;
 
 use graphics::{
-    matrix::{projections, transform, Matrix},
     canvas::Canvas,
+    matrix::{projections, transform, Matrix},
     PPMImg,
 };
 
@@ -16,12 +16,11 @@ fn main() {
     let mut mv = 150.;
     for i in 1..=total {
         let mut model = Matrix::new_polygon_matrix();
-        model.add_sphere((130., 110., 100.), 120.);
-        model.add_sphere((-130., 100., 100.), 120.);
-        model.add_box((-60., -60., 100.), 90., 90., 90.);
-        model.add_torus((-30., -335., 100.), 25., 200.);
+        model.add_sphere((130., 110., 90.), 120.);
+        model.add_sphere((-130., 100., 90.), 120.);
+        model.add_box((-60., -60., 50.), 90., 90., 90.);
+        model.add_torus((-30., -335., 90.), 25., 200.);
 
-        
         let step = 150.;
         if i < 6 {
             mv += step
@@ -30,8 +29,8 @@ fn main() {
         };
 
         model *= Matrix::ident(4)
-            * transform::rotatey(10. * i as f64 - total as f64 * 5.)
-            * transform::mv(0., 0., mv);
+            * transform::mv(0., 0., mv)
+            * transform::rotatey(10. * i as f64 - total as f64 * 5.);
 
         // now apply perspective
         let mut model = model * projections::perspective(90., 1., 1., 600.);
